af9431454b0122628bcf22eb5924537e19a9a7ca
yuhaiqiang <yuhaiqiangvip@sina.com>
2019-08-27 22:53:21 +0800

    新增pre-commit

---
+++ bin/generate_xml.py
@@ -0,0 +1,11 @@
+import time
+import os
+filePath = './'
+for i,j,k in os.walk(filePath):
+    for f in k:
+    	if ".xmind" in f:
+    		f_name="%s/%s"%  (i, f)
+    		f_name=f_name.replace(" ","\\ ")
+    		f_name=f_name.replace("&", "\\&")
+    		shell_cmd = "xmindparser %s -xml" % f_name
+    		os.system(shell_cmd)
